fb048c01ab63a32c6d19a0bd71d7fc76d08c6902
Gvvsatyanarayana <95292883+Gvvsatyanarayana@users.noreply.github.com>
2022-03-05 14:21:30 +0530

    import java.util.*; class a{     public static void main(String arg[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n==1)         System.out.println('0');         else if(n==2)         System.out.println('1');         //if(n>2)         else             System.out.println('2');     } }

---
+++ The_Pandemic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class a{
+    public static void main(String arg[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==1)
+        System.out.println('0');
+        else if(n==2)
+        System.out.println('1');
+        //if(n>2)
+        else
+            System.out.println('2');
+    }
+}
